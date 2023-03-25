68e8c8490564d0fddfef5ec331f5160712517e14
Thomas Newnum <thomas.newnum@gmail.com>
2023-03-25 13:52:23 -0400

    header is now sticky!

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+let header = document.getElementById("myHeader");
+console.log(header)
+let sticky = header.offsetTop;
+
+
+window.onscroll = function() {
+    scrollFunc()
+};
+
+function scrollFunc () {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+};
